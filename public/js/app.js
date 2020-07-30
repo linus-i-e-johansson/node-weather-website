8aa4c5fbe6b371c19dcb7baa987558c6a4ba0f71
Linus-Johansson <40174194+Linus-Johansson@users.noreply.github.com>
2020-07-30 16:08:47 +0200

    Setup app for heroku

--- public/js/app.js
+++ public/js/app.js
@@ -9,20 +9,18 @@ weatherForm.addEventListener("submit", (e) => {
   messageOne.textContent = "Loading...";
   messageTwo.textContent = "";
 
-  fetch(`http://localhost:3000/weather?address=${location}`).then(
-    (response) => {
-      response.json().then((data) => {
-        console.log(data);
-        if (data.error) {
-          messageOne.textContent = data.error;
-        } else {
-          messageOne.textContent = `Here is the weather forcast for ${data.location}`;
-          messageTwo.textContent = ` 
+  fetch(`/weather?address=${location}`).then((response) => {
+    response.json().then((data) => {
+      console.log(data);
+      if (data.error) {
+        messageOne.textContent = data.error;
+      } else {
+        messageOne.textContent = `Here is the weather forcast for ${data.location}`;
+        messageTwo.textContent = ` 
             The temprature is ${data.temprature} 
             but feels like ${data.feelslike}
             and there is ${data.precip} % chance for rain`;
-        }
-      });
-    }
-  );
+      }
+    });
+  });
 });
